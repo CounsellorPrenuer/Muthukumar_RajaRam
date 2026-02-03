da23fa645f6aa195584a975f1b0698c8c00f183a
Mihika Sarwate <mihika@example.com>
2026-02-03 21:28:49 +0530

    Fix basePath to match repo name DC_JOHN

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/DC-John',
+  basePath: '/DC_JOHN',
   reactStrictMode: true,
   images: {
     unoptimized: true,
